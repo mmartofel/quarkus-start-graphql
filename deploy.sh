b360ae6d48a468d1eba230f6133c5b3a339dba8b
mmartofel <mmartofe@redhat.com>
2020-12-18 00:34:59 +0100

    default-profile

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@
  oc delete deployment/quarkus-start-graphql
  oc delete buildconfig/quarkus-start-graphql
  oc delete imagestream/quarkus-start-graphql
+ oc delete route/quarkus-start-graphql
  oc new-app https://github.com/mmartofel/quarkus-start-graphql.git
  oc logs -f bc/quarkus-start-graphql
+oc expose svc/quarkus-start-graphql
