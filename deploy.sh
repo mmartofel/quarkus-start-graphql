39687d660eac1590a6a2bb5bf57f4a8f579d5b67
mmartofel <mmartofe@redhat.com>
2020-12-18 00:06:12 +0100

    cpllation

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+ mvn package
+ git add .
+ git commit -m $1
+ git push origin main
+ oc delete svc/quarkus-start-graphql
+ oc delete deployment/quarkus-start-graphql
+ oc delete buildconfig/quarkus-start-graphql
+ oc delete imagestream/quarkus-start-graphql
+ oc new-app https://github.com/mmartofel/quarkus-start-graphql.git
+ oc logs -f bc/quarkus-start-graphql
