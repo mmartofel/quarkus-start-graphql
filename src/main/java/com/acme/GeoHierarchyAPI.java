782dfc523698b9e30ad35e49d684afb7f6c15914
mmartofel <mmartofe@redhat.com>
2021-04-22 23:24:36 +0200

    Configuration change done at: Thu 22 Apr 2021 23:24:16 CEST

--- src/main/java/com/acme/GeoHierarchyAPI.java
+++ src/main/java/com/acme/GeoHierarchyAPI.java
@@ -4,40 +4,50 @@ import java.util.List;
 import javax.inject.Inject;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
+import org.jboss.logging.Logger;
+
 
 @GraphQLApi
 public class GeoHierarchyAPI {
+
+    private static final Logger LOG = Logger.getLogger(GeoHierarchyAPI.class);
 
 @Inject
 GeoHierarchyService service;
 
 @Query
 public GeoHierarchy getOneGeo(int Id){
+    LOG.info("Single geo location requested");
     return service.getOneGeo(Id);
 }
 
 @Query
 public List<GeoHierarchy> getAllGeo(){
+    LOG.info("ALL! geo locations requested");
     return service.getAllGeo();
 }
 
 @Query
 public List<String> getAllWojewodztwo(){
+    LOG.info("First page of an app hit by new user :-) ");
     return service.getAllWojewodztwo();
 }
 
 @Query
 public List<String> getAllPowiat(String Wojewodztwo){
+    LOG.info("List of POWIAT requested");
     return service.getAllPowiat(Wojewodztwo);
 }
 
 @Query
 public List<String> getAllGmina(String Wojewodztwo, String Powiat){
+    LOG.info("List of GMINA requested");
     return service.getAllGmina(Wojewodztwo,Powiat);
 }
 
 @Query
 public List<String> getAllMiejscowosc(String Wojewodztwo, String Powiat, String Gmina){
+    LOG.info("List of MIEJSCOWOSC requested");
     return service.getAllMiejscowosc(Wojewodztwo,Powiat,Gmina);
 }
 
